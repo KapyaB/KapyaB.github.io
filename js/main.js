79d96089490019389cee4ec6eb43948db651292b
Bright <kapyabright@outlook.com>
2019-06-19 22:36:55 +0200

    ui for outing zm

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function revealReplies(review_id) {
+  const replies = document.getElementById(review_id);
+
+  replies.classList.toggle("toggle-replies");
+}
+
+// When the user clicks the button, open the modal
+function revealProducts() {
+  var modal = document.getElementById("productModal");
+  var mainSection = document.getElementsByClassName("business-profile")[0];
+  modal.style.display = "block";
+  mainSection.style.opacity = 0.3;
+}
+
+function closeModal() {
+  var modal = document.getElementById("productModal");
+  var mainSection = document.getElementsByClassName("business-profile")[0];
+  modal.style.display = "none";
+  mainSection.style.opacity = 1;
+}
+
+// enlarge showcase image
+function enlarge(id) {
+  const image = document.getElementById(`${id}`);
+  image.classList.toggle("enlarged");
+  // image.style.maxHeight = "80%";
+}
+
+// promo slideshow
+function slideshow() {
+  var i;
+  var x = document.getElementsByClassName("promo");
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+
+  index++;
+
+  if (index > x.length) {
+    index = 1;
+  }
+
+  x[index - 1].style.display = "grid";
+  setTimeout(slideshow, 5000); // 3 secs
+}
